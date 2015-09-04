5e2d85778b541a7287ae5a164c6e3eebfb88e1fa
dxlab <ds@dxlab.com.ua>
2015-09-05 00:28:39 +0300

    mLazyValueA = 10

--- app/src/main/java/com/example/d_rom/supportdesigndemo/TestLazyClass.java
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/TestLazyClass.java
@@ -5,7 +5,7 @@ package com.example.d_rom.supportdesigndemo;
  */
 public class TestLazyClass {
 
-    private int mLazyValueA = 3;
+    private int mLazyValueA = 10;
     private int mLazyValueB = 3000;
 
     public int getLazyValueA() {
