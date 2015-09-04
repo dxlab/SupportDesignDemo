5b6c9d3109f6d422caf8655b954eab16fb4a864d
dxlab <ds@dxlab.com.ua>
2015-09-05 00:23:06 +0300

    Fileds mLazyValueA, mLazyValueB created

--- app/src/main/java/com/example/d_rom/supportdesigndemo/TestLazyClass.java
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/TestLazyClass.java
@@ -4,4 +4,8 @@ package com.example.d_rom.supportdesigndemo;
  * Created by Dima on 05.09.2015.
  */
 public class TestLazyClass {
+
+    private int mLazyValueA = 1;
+    private int mLazyValueB = 1000;
+
 }
