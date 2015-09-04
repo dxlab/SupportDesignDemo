95f82f1b70c08729f49b6b68e81a890963ac7af3
dxlab <ds@dxlab.com.ua>
2015-09-05 00:24:45 +0300

    proper getters and setters generated

--- app/src/main/java/com/example/d_rom/supportdesigndemo/TestLazyClass.java
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/TestLazyClass.java
@@ -8,4 +8,21 @@ public class TestLazyClass {
     private int mLazyValueA = 1;
     private int mLazyValueB = 1000;
 
+    public int getLazyValueA() {
+        return mLazyValueA;
+    }
+
+    public void setLazyValueA(int _lazyValueA) {
+        this.mLazyValueA = _lazyValueA;
+    }
+
+    public int getLazyValueB() {
+        return mLazyValueB;
+    }
+
+    public void setLazyValueB(int _lazyValueB) {
+        this.mLazyValueB = _lazyValueB;
+    }
+
+
 }
